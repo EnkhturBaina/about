4c02576327a779a357bad0d61e2610a946aea489
enkhturBaina <eegii.111.gg@gmail.com>
2025-01-23 10:54:12 +0800

    zaswar

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -2,21 +2,16 @@ import React from "react";
 import Typewriter from "typewriter-effect";
 
 function Type() {
-  return (
-    <Typewriter
-      options={{
-        strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
-      }}
-    />
-  );
+	return (
+		<Typewriter
+			options={{
+				strings: ["Mobile Developer", "Android & IOS", ""],
+				autoStart: true,
+				loop: true,
+				deleteSpeed: 50
+			}}
+		/>
+	);
 }
 
 export default Type;
