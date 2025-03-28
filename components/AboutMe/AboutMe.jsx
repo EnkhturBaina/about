feb56233128ba7493592971d3acfd31fbe288923
enkhturBaina <eegii.111.gg@gmail.com>
2025-03-28 16:45:11 +0800

    xxx

--- components/AboutMe/AboutMe.jsx
+++ components/AboutMe/AboutMe.jsx
@@ -6,7 +6,7 @@ function AboutMe({ route }) {
 			<section id="about" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="about me">
 				<div className="text-slate-500">
 					<p className=" mb-2">
-						<span className="hover-highlight">6+</span> years of experience...{" "}
+						<span className="hover-highlight">6+</span> years of experience{" "}
 					</p>
 					<p className=" mb-2">
 						I am a <span className="hover-highlight">Mobile & Front End developer</span> that based in Ulaanbaatar,
