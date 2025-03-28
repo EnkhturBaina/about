6db867f88389bd912cace9e1f4ce00ab36a676fa
enkhturBaina <eegii.111.gg@gmail.com>
2025-03-28 16:32:44 +0800

    Deploying to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
 	output: "export", // Static export горим
 	basePath: "/about", // Repo-н нэртэй таарах ёстой
-	assetPrefix: "/about/", // Статик файлууд зөв дуудагдах ёстой
 	images: {
 		unoptimized: true // GitHub Pages Next.js-ийн зураг оновчлолыг дэмждэггүй
 	}
