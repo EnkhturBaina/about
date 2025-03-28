383778de707b1041dce4fbbdb10fc83c30abc1f7
enkhturBaina <eegii.111.gg@gmail.com>
2025-03-28 17:27:41 +0800

    xxx

--- components/ProjectItem/ProjectItem.jsx
+++ components/ProjectItem/ProjectItem.jsx
@@ -10,7 +10,7 @@ function ProjectItem({ thumbnail, title, description, tags, demo }) {
 				<div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
 
 				<img
-					src={require("../utils/app_ss/autohuleg.png")}
+					src="/images/autohuleg.png"
 					alt="Build a Spotify Connected App Newline course marketing card"
 					loading="lazy"
 					width="200"
